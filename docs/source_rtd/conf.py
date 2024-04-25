58e08aab6cd7338bb1cd27d894f424f312705319
kpich <karlpichotta@gmail.com>
2024-04-25 14:24:55 -0400

    linter on docs/ py

--- docs/source_rtd/conf.py
+++ docs/source_rtd/conf.py
@@ -20,11 +20,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.autodoc", 
-    "sphinx.ext.napoleon",
-    "sphinx_rtd_theme"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 autodoc_default_options = {
     "members": True,
@@ -45,4 +41,3 @@ sys.path.append(Path(__file__).parents[2].resolve())
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-
