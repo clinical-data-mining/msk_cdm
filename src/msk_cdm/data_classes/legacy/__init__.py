39d4a8ace87c7cea6e42d4b3a1c340b5ebb42dd2
Chris Fong <fongc2@mskcc.org>
2024-05-30 13:32:58 -0400

    Deprecating use of cbioportal variable. Information has moved to the cdm-cbioportal-etl repo

--- src/msk_cdm/data_classes/legacy/__init__.py
+++ src/msk_cdm/data_classes/legacy/__init__.py
@@ -1,11 +1,9 @@
 from ._data_classes_cdm import (
     CDMProcessingVariables,
-    CDMProcessingVariablesCbioportal,
     CDMRedcapConfig,
 )
 
 __all__ = [
     "CDMProcessingVariables",
-    "CDMProcessingVariablesCbioportal",
     "CDMRedcapConfig",
 ]
