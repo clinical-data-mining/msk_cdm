fe126e9145917d2bb6251e35c0a0a61bb4aa4a6c
Chris Fong <fongc2@mskcc.org>
2024-06-06 16:03:58 -0400

    Adding exception for databricks api

--- src/msk_cdm/databricks/_databricks_api.py
+++ src/msk_cdm/databricks/_databricks_api.py
@@ -93,12 +93,14 @@ class DatabricksAPI(object):
             # Re-raise the exception for further handling if necessary self._client = client
     def _connect_sql(self):
         try:
-            client = sql.connect(
-                server_hostname=self._HOSTNAME,
-                http_path=self._HTTP_PATH,
-                access_token=self._TOKEN
-            )
-            print("Connected successfully.")
+            with timeout(10):  # Timeout in 10 seconds
+                client = sql.connect(
+                    server_hostname=self._HOSTNAME,
+                    http_path=self._HTTP_PATH,
+                    token=self._TOKEN
+                )
+                print("Connected successfully.")
+                self._client = client
         except Exception as e:
             print('Client connection failed:', str(e))
             logger.error('Connection failed with exception: %s', str(e))
