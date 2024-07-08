78e68c53afbcec578ef560f299aeb9114e0146ae
Chris Fong <fongc2@mskcc.org>
2024-07-08 17:07:06 -0400

    Adding code to package

--- src/msk_cdm/databricks/databricks_api.py
+++ src/msk_cdm/databricks/databricks_api.py
@@ -22,16 +22,24 @@ os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 class DatabricksAPI(object):
     def __init__(
             self,
-            token,
-            hostname,
-            http_path
+            token=None,
+            hostname=None,
+            http_path=None,
+            fname_databricks_env=None
     ):
-
+        self._TOKEN = token
+        self._HOSTNAME = hostname
+        self._HTTP_PATH = http_path
         self._client = None
+
+        if fname_databricks_env is not None:
+            print('Parsing env file')
+            self._process_env()
+
         self._connect(
-            token=token,
-            hostname=hostname,
-            http_path=http_path
+            token=self._TOKEN,
+            hostname=self._HOSTNAME,
+            http_path=self._HTTP_PATH
         )
 
     def _connect(self, token, hostname, http_path):
@@ -44,6 +52,24 @@ class DatabricksAPI(object):
         print('Connected.')
 
         self._client = connection
+
+        return None
+
+    def _process_env(
+            self,
+            fname_databricks_env
+    ):
+
+        dict_config = dotenv_values(fname_databricks_env)
+
+        if not self._TOKEN:
+            self._TOKEN = dict_config.get("TOKEN", None)
+        if not self._HOSTNAME:
+            self._HOSTNAME = dict_config.get("HOSTNAME", None)
+        if not self._URL:
+            self._URL = dict_config.get("URL", None)
+        if not self._HTTP_PATH:
+            self._HTTP_PATH = dict_config.get("HTTP_PATH", None)
 
         return None
 
@@ -104,3 +130,4 @@ class DatabricksAPI(object):
         )
 
         return df
+
