d90f73c7a7e10cbf9c04bd89cfb6b30c80ffe8eb
fongc2 <fongc2@mskcc.org>
2024-08-29 15:13:13 -0400

    Adding code to push data back into databricks

--- src/msk_cdm/databricks/databricks_api.py
+++ src/msk_cdm/databricks/databricks_api.py
@@ -1,9 +1,18 @@
 import os, logging
-from databricks import sql
-import pandas as pd
-import certifi
 import pathlib
 from dotenv import dotenv_values
+from io import BytesIO, StringIO
+
+from databricks import sql
+from databricks.sdk import WorkspaceClient
+from mkdocs.config.config_options import Optional
+from sqlalchemy import (
+    create_engine,
+    URL
+)
+import pandas as pd
+import certifi
+
 
 cwd = pathlib.Path(__file__).parent.resolve()
 
@@ -49,8 +58,9 @@ class DatabricksAPI(object):
         self._TOKEN = token
         self._HOSTNAME = hostname
         self._HTTP_PATH = http_path
-        self._client = None
+        self._sql_client = None
         self._URL = None
+        self._workspace_client = None
 
         if fname_databricks_env is not None:
             print('Parsing env file')
@@ -91,9 +101,17 @@ class DatabricksAPI(object):
             http_path=http_path,
             access_token=token
         )
+
+        workspace_client = WorkspaceClient(
+            host=hostname,
+            token=token
+        )
+
         print('Connected.')
 
-        self._client = connection
+        self._sql_client = connection
+        self._workspace_client = workspace_client
+
 
         return None
 
@@ -180,7 +198,7 @@ class DatabricksAPI(object):
            A DataFrame containing the results of the query.
         """
 
-        cursor = self._client.cursor()
+        cursor = self._sql_client.cursor()
         for i,query in enumerate(sql.split(';')):
             cursor.execute(query)
 
@@ -196,6 +214,117 @@ class DatabricksAPI(object):
 
         return df
 
+    def read_db_obj(
+            self,
+            volume_path: str,
+            sep: Optional[str] ='\t'
+    ) -> pd.DataFrame:
+        # Read csv/tsv file from volume and convert into Pandas dataframe
+        response = self._workspace_client.files.download(volume_path)
+        data_str = BytesIO(response.contents.read())
+        df = pd.read_csv(data_str, sep=sep)
+
+        return df
+
+    def write_db_obj(
+            self,
+            df: pd.DataFrame,
+            volume_path: str,
+            sep: Optional[str] = '\t',
+            overwrite: Optional[bool] = True,
+            dict_database_table_info: Optional[dict] = None
+    ):
+        """
+        If `dict_database_table_info` is used, it must contain these keys
+        - catalog: Databricks catalog used
+        - schema: Schema within the catalog
+        - table: Table in the schema that will contain the dataframe information
+        - volume_path: Path location on the volume of the object. A csv file for use of this
+        - sep_of_volume_obj: File separator used for the object. Typically, comma or tab separated
+
+        """
+
+        csv_bytes = df.to_csv(index=False, sep=sep).encode("utf-8")
+        csv_buffer = BytesIO(csv_bytes)
+
+        print('Writing to %s' % volume_path)
+        self._workspace_client.files.upload(
+            volume_path,
+            csv_buffer,
+            overwrite=overwrite
+        )
+        print('Write to volume complete')
+
+        if dict_database_table_info is not None:
+
+            if sep != dict_database_table_info.get('sep'):
+                dict_database_table_info['sep'] = sep
+                print("Conflict with separator in dict; setting to value object was saved as.")
+
+            self.create_table_from_volume(
+                connection=self._workspace_client,
+                dict_database_table_info=dict_database_table_info
+            )
+
+        return None
+
+    def _sql_write_creator(
+            self,
+            catalog,
+            schema,
+            table,
+            volume_path,
+            sep='\t'
+    ):
+        sql_write = f"""
+        DROP TABLE IF EXISTS {catalog}.{schema}.{table};
+        CREATE TABLE IF NOT EXISTS {catalog}.{schema}.{table};
+       
+        COPY INTO {catalog}.{schema}.{table} FROM (
+        SELECT *, _metadata FROM '{volume_path}'
+        ) 
+        FILEFORMAT = CSV 
+        FORMAT_OPTIONS ('delimiter' = '{sep}', 'header' = 'true') 
+        COPY_OPTIONS ('mergeSchema' = 'true');
+        
+        """
+
+        return sql_write
+
+    def create_table_from_volume(
+            self,
+            dict_database_table_info
+    ):
+        catalog = dict_database_table_info.get('catalog')
+        schema = dict_database_table_info.get('schema')
+        table = dict_database_table_info.get('table')
+        volume_path = dict_database_table_info.get('volume_path')
+        sep_of_volume_obj = dict_database_table_info.get('sep')
+        print('Creating SQL table from volume:')
+        print('Catalog: %s' % catalog)
+        print('Schema: %s' % schema)
+        print('Table: %s' % table)
+        print('Volume path: %s' % volume_path)
+        print('Separator: %s' % sep_of_volume_obj)
+
+        sql_write = self._sql_write_creator(
+            catalog=catalog,
+            schema=schema,
+            table=table,
+            volume_path=volume_path,
+            sep=sep_of_volume_obj
+        )
+
+        cursor = self._sql_client.cursor()
+        for i,query in enumerate(sql_write.split(';')[:-1]):
+            print(query)
+            cursor.execute(query)
+
+        print('Table created')
+
+        return None
+
+
     def close_connection(self):
         """
         Closes the connection to the Databricks cluster.
@@ -204,9 +333,9 @@ class DatabricksAPI(object):
         --------
         None
         """
-        cursor = self._client.cursor()
+        cursor = self._sql_client.cursor()
         cursor.close()
-        self._client.close()
+        self._sql_client.close()
         print('Databricks connection closed')
 
         return None
